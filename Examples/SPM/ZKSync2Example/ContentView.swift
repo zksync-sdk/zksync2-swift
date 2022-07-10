acd342878e63fcc80fb66d01f940dd9ebe7b9139
Maxim Makhun <mmakhun@vareger.com>
2022-07-10 16:56:25 -0700

    Add SPM example application.

---
+++ Examples/SPM/ZKSync2Example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZKSync2Example
+//
+//  Created by Maksim Makhun on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

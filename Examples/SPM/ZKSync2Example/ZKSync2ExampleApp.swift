8e18477391ecddc100f3eeea9f96a33ba084e646
Maxim Makhun <mmakhun@vareger.com>
2022-07-10 18:04:27 -0700

    Import `ZKSync2` in SPM example application.

--- Examples/SPM/ZKSync2Example/ZKSync2ExampleApp.swift
+++ Examples/SPM/ZKSync2Example/ZKSync2ExampleApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ZKSync2
 
 @main
 struct ZKSync2ExampleApp: App {
--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ZKSync2",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "ZKSync2",

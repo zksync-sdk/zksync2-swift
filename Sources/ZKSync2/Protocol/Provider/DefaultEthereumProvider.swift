c90935e6b3ff1c0de0ebe42084cfabbf8d9f48a2
Maxim Makhun <mmakhun@vareger.com>
2022-07-17 21:14:36 -0700

    Implement `EthereumProvider` protocol.

--- Sources/ZKSync2/Protocol/Provider/DefaultEthereumProvider.swift
+++ Sources/ZKSync2/Protocol/Provider/DefaultEthereumProvider.swift
@@ -6,13 +6,50 @@
 //
 
 import Foundation
+import BigInt
 import web3swift
 
 public class DefaultEthereumProvider: EthereumProvider {
     
+    public func approveDeposits(with token: Token,
+                                limit: BigUInt?,
+                                completion: @escaping (Result<TransactionReceipt, Error>) -> Void) {
+        
+    }
+    
+    public func transfer(with token: Token,
+                         amount: BigUInt,
+                         to address: String,
+                         completion: @escaping (Result<TransactionReceipt, Error>) -> Void) {
+        
+    }
+    
+    public func deposit(with token: Token,
+                        amount: BigUInt,
+                        to userAddress: String,
+                        completion: @escaping (Result<TransactionReceipt, Error>) -> Void) {
+        
+    }
+    
+    public func withdraw(with token: Token,
+                         amount: BigUInt,
+                         from userAddress: String,
+                         completion: @escaping (Result<TransactionReceipt, Error>) -> Void) {
+        
+    }
+    
+    public func isDepositApproved(with token: Token,
+                                  address: String,
+                                  threshold: BigUInt?,
+                                  completion: @escaping (Result<Bool, Error>) -> Void) {
+        
+    }
+    
+    public var contractAddress: String = ""
+    
     let web3: web3
     
-    init(web3: web3) {
+    init(_ web3: web3) {
         self.web3 = web3
     }
 }
--- Sources/ZKSync2/Protocol/Provider/EthereumProvider.swift
+++ Sources/ZKSync2/Protocol/Provider/EthereumProvider.swift
@@ -6,7 +6,65 @@
 //
 
 import Foundation
+import BigInt
+import web3swift
 
 public protocol EthereumProvider {
     
+    /// Send approve transaction to token contract.
+    /// - Parameters:
+    ///   - token: Token object supported by ZkSync.
+    ///   - limit: Maximum amount to approve for ZkSync contract.
+    ///   - completion: The completion handler to execute after finishing transaction.
+    func approveDeposits(with token: Token,
+                         limit: BigUInt?,
+                         completion: @escaping (Result<TransactionReceipt, Error>) -> Void)
+    
+    /// Send transfer transaction. This is the regular transfer of ERC20 token.
+    /// - Parameters:
+    ///   - token: Token object supported by ZkSync.
+    ///   - amount: Amount of tokens to transfer.
+    ///   - address: Tokens receiver address.
+    ///   - completion: The completion handler to execute after finishing transaction.
+    func transfer(with token: Token,
+                  amount: BigUInt,
+                  to address: String,
+                  completion: @escaping (Result<TransactionReceipt, Error>) -> Void)
+    
+    /// Send deposit transaction to ZkSync contract. For ERC20 token must be approved beforehand
+    /// using `EthereumProvider.approveDeposits()`.
+    /// - Parameters:
+    ///   - token: Token object supported by ZkSync.
+    ///   - amount: Amount of tokens to transfer.
+    ///   - userAddress: Address of L2 deposit receiver in ZkSync.
+    ///   - completion: The completion handler to execute after finishing transaction.
+    func deposit(with token: Token,
+                 amount: BigUInt,
+                 to userAddress: String,
+                 completion: @escaping (Result<TransactionReceipt, Error>) -> Void)
+    
+    /// Send withdraw transaction to ZkSync contract.
+    /// - Parameters:
+    ///   - token: Token object supported by ZkSync.
+    ///   - amount: Address of the account who can deposit tokens from yours.
+    ///   - userAddress: Address of L1 withdrawal receiver in ZkSync.
+    ///   - completion: The completion handler to execute after finishing transaction.
+    func withdraw(with token: Token,
+                  amount: BigUInt,
+                  from userAddress: String,
+                  completion: @escaping (Result<TransactionReceipt, Error>) -> Void)
+    
+    /// Check if deposit is approved.
+    /// - Parameters:
+    ///   - token: Token object supported by ZkSync.
+    ///   - address: Address of the account who can deposit tokens from yours.
+    ///   - threshold: Minimum threshold of approved tokens.
+    ///   - completion: The completion handler to execute after finishing transaction.
+    func isDepositApproved(with token: Token,
+                           address: String,
+                           threshold: BigUInt?,
+                           completion: @escaping (Result<Bool, Error>) -> Void)
+    
+    /// ZkSync smart-contract address in Ethereum blockchain.
+    var contractAddress: String { get }
 }

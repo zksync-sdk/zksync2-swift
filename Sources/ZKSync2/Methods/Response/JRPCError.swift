401768a41e4a1c0275587fb8162788e8ef4beddd
Maxim Makhun <mmakhun@vareger.com>
2022-07-19 21:25:58 -0700

    Add `Response` and `Error` structs for RPC requests.

---
+++ Sources/ZKSync2/Methods/Response/JRPCError.swift
@@ -0,0 +1,20 @@
+//
+//  Error.swift
+//  ZKSync2
+//
+//  Created by Maxim Makhun on 7/17/22.
+//
+
+import Foundation
+
+extension JRPC {
+    
+    struct Error: Codable {
+        
+        /// Code of the error.
+        let code: Int
+        
+        /// Error message.
+        let message: String
+    }
+}
---
+++ Sources/ZKSync2/Methods/Response/JRPCResponse.swift
@@ -0,0 +1,26 @@
+//
+//  Response.swift
+//  ZKSync2
+//
+//  Created by Maxim Makhun on 7/17/22.
+//
+
+import Foundation
+
+struct JRPC {
+    
+    struct Response<T: Decodable>: Decodable {
+        
+        /// JSON-RPC identifier.
+        let id: Int
+        
+        /// JSON-RPC version. Typically `2.0`.
+        let jsonrpc: String
+        
+        /// JSON-RPC result (if present).
+        let result: T?
+        
+        /// Error (if present).
+        let error: Error?
+    }
+}

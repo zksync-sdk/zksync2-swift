50950f81659b5aefc2a7ba163997fd884fe66bcd
Maxim Makhun <mmakhun@vareger.com>
2022-07-28 14:26:44 -0700

    Add `EthSigner` and `PrivateKeyEthSigner` placeholders.

---
+++ Sources/ZKSync2/Crypto/EthSigner.swift
@@ -0,0 +1,12 @@
+//
+//  EthSigner.swift
+//  ZKSync2
+//
+//  Created by Maxim Makhun on 7/23/22.
+//
+
+import Foundation
+
+protocol EthSigner {
+    
+}
---
+++ Sources/ZKSync2/Crypto/PrivateKeyEthSigner.swift
@@ -0,0 +1,12 @@
+//
+//  PrivateKeyEthSigner.swift
+//  ZKSync2
+//
+//  Created by Maxim Makhun on 7/23/22.
+//
+
+import Foundation
+
+public class PrivateKeyEthSigner: EthSigner {
+    
+}

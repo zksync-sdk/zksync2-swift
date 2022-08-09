4596c252666e1bb0aad1d8575332c4fc4112a5af
Maxim Makhun <mmakhun@vareger.com>
2022-08-08 22:18:07 -0600

    Remove methods that are not needed. Add `ZkSyncNetwork`.

---
+++ Sources/ZKSync2/Protocol/Core/ZkSyncNetwork.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maksim Makhun on 7/28/22.
+//
+
+import Foundation
+
+enum ZkSyncNetwork: Int {
+    
+    case mainnet = 1
+    case localhost = 42
+    case unknown
+}

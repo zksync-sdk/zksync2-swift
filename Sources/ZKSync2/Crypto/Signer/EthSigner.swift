946bebcffb953cb71733f76c8ec25066b911bc98
Maxim Makhun <mmakhun@vareger.com>
2022-09-04 15:30:45 -0700

    Add `EthSigner` and `PrivateKeyEthSigner` placeholders.

---
+++ Sources/ZKSync2/Crypto/Signer/EthSigner.swift
@@ -0,0 +1,117 @@
+//
+//  EthSigner.swift
+//  ZKSync2
+//
+//  Created by Maxim Makhun on 7/23/22.
+//
+
+import Foundation
+import web3swift
+
+//public interface EthSigner {
+//
+//    static final String MESSAGE_PREFIX = "\u0019Ethereum Signed Message:\n";
+//    static final String MESSAGE_EIP712_PREFIX = "\u0019\u0001";
+//    static final byte[] EIP1271_SUCCESS_VALUE = Numeric.hexStringToByteArray("0x1626ba7e");
+//
+//    /**
+//     * Get wallet address
+//     *
+//     * @return Address in hex string
+//     */
+//    String getAddress();
+//
+//    /**
+//     * Get EIP712 domain
+//     *
+//     * @return Eip712 domain
+//     */
+//    CompletableFuture<Eip712Domain> getDomain();
+//
+//    /**
+//     * Signs typed struct using ethereum private key by EIP-712 signature standard.
+//     *
+//     * @param <S> - EIP712 structure
+//     * @param domain - EIP712 domain
+//     * @param typedData - Object implementing EIP712 structure standard
+//     * @return Signature object
+//     */
+//    <S extends Structurable> CompletableFuture<String> signTypedData(Eip712Domain domain, S typedData);
+//
+//    /**
+//     * Verify typed EIP-712 struct standard.
+//     *
+//     * @param <S> - EIP712 structure
+//     * @param domain - EIP712 domain
+//     * @param typedData - Object implementing EIP712 structure standard
+//     * @param signature - Signature of the EIP-712 structures
+//     * @return true on verification success
+//     */
+//    <S extends Structurable> CompletableFuture<Boolean> verifyTypedData(Eip712Domain domain, S typedData, String signature);
+//
+//    /**
+//     * Sign raw message
+//     *
+//     * @param message - Message to sign
+//     * @return Signature object
+//     */
+//    CompletableFuture<String> signMessage(byte[] message);
+//
+//    /**
+//     * Sign raw message
+//     *
+//     * @param message - Message to sign
+//     * @param addPrefix - If true then add secure prefix (https://eips.ethereum.org/EIPS/eip-712)
+//     * @return Signature object
+//     */
+//    CompletableFuture<String> signMessage(byte[] message, boolean addPrefix);
+//
+//    /**
+//     * Verify signature with raw message
+//     *
+//     * @param signature - Signature object
+//     * @param message - Message to verify
+//     * @return true on verification success
+//     */
+//    CompletableFuture<Boolean> verifySignature(String signature, byte[] message);
+//
+//    /**
+//     * Verify signature with raw message
+//     *
+//     * @param signature - Signature object
+//     * @param message - Message to verify
+//     * @param prefixed - If true then add secure prefix (https://eips.ethereum.org/EIPS/eip-712)
+//     * @return true on verification success
+//     */
+//    CompletableFuture<Boolean> verifySignature(String signature, byte[] message, boolean prefixed);
+//
+//    static byte[] getEthereumMessagePrefix(int messageLength) {
+//        return MESSAGE_PREFIX.concat(String.valueOf(messageLength)).getBytes();
+//    }
+//
+//    static byte[] getEthereumMessageHash(byte[] message) {
+//        byte[] prefix = getEthereumMessagePrefix(message.length);
+//
+//        byte[] result = new byte[prefix.length + message.length];
+//        System.arraycopy(prefix, 0, result, 0, prefix.length);
+//        System.arraycopy(message, 0, result, prefix.length, message.length);
+//
+//        return Hash.sha3(result);
+//    }
+//}
+
+// ZKSync2 (Java): EthSigner.java
+// ZKSync (Swift): EthSigner.swift
+protocol EthSigner {
+    
+//    // String getAddress();
+//    var address: String { get }
+//    
+//    // CompletableFuture<Eip712Domain> getDomain();
+//    var domain: Eip712Domain { get }
+//    
+//    // <S extends Structurable> CompletableFuture<String> signTypedData(Eip712Domain domain, S typedData);
+//    func signTypedData(_ domain: Eip712Domain)
+    
+//    var domain: EIP712Domain {}
+}
---
+++ Sources/ZKSync2/Crypto/Signer/PrivateKeyEthSigner.swift
@@ -0,0 +1,125 @@
+//
+//  PrivateKeyEthSigner.swift
+//  ZKSync2
+//
+//  Created by Maxim Makhun on 7/23/22.
+//
+
+import Foundation
+
+//@AllArgsConstructor
+//public class PrivateKeyEthSigner implements EthSigner {
+//
+//    private Credentials credentials;
+//    private Long chainId;
+//
+//    public static PrivateKeyEthSigner fromMnemonic(String mnemonic, long chainId) {
+//        Credentials credentials = generateCredentialsFromMnemonic(mnemonic, 0);
+//        return new PrivateKeyEthSigner(credentials, chainId);
+//    }
+//
+//    public static PrivateKeyEthSigner fromMnemonic(String mnemonic, int accountIndex, long chainId) {
+//        Credentials credentials = generateCredentialsFromMnemonic(mnemonic, accountIndex);
+//        return new PrivateKeyEthSigner(credentials, chainId);
+//    }
+//
+//    @Override
+//    public String getAddress() {
+//        return credentials.getAddress();
+//    }
+//
+//    @Override
+//    public CompletableFuture<Eip712Domain> getDomain() {
+//        Eip712Domain domain = Eip712Domain.defaultDomain(chainId);
+//        return CompletableFuture.completedFuture(domain);
+//    }
+//
+//    @Override
+//    public <S extends Structurable> CompletableFuture<String> signTypedData(Eip712Domain domain, S typedData) {
+//        return this.signMessage(Eip712Encoder.typedDataToSignedBytes(domain, typedData), false);
+//    }
+//
+//    @Override
+//    public <S extends Structurable> CompletableFuture<Boolean> verifyTypedData(Eip712Domain domain, S typedData,
+//            String signature) {
+//        return this.verifySignature(signature, Eip712Encoder.typedDataToSignedBytes(domain, typedData), false);
+//    }
+//
+//    @Override
+//    public CompletableFuture<String> signMessage(byte[] message) {
+//        return this.signMessage(message, true);
+//    }
+//
+//    @Override
+//    public CompletableFuture<String> signMessage(byte[] message, boolean addPrefix) {
+//        Sign.SignatureData sig = addPrefix ? Sign.signPrefixedMessage(message, credentials.getEcKeyPair())
+//                : Sign.signMessage(message, credentials.getEcKeyPair(), false);
+//
+//        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+//
+//        try {
+//            output.write(sig.getR());
+//            output.write(sig.getS());
+//            output.write(sig.getV()[0] - 27);
+//        } catch (IOException e) {
+//            throw new IllegalStateException("Error when creating ETH signature", e);
+//        }
+//
+//        final String signature = Numeric.toHexString(output.toByteArray());
+//
+//        return CompletableFuture.completedFuture(signature);
+//    }
+//
+//    @Override
+//    public CompletableFuture<Boolean> verifySignature(String signature, byte[] message) {
+//        return this.verifySignature(signature, message, true);
+//    }
+//
+//    @Override
+//    public CompletableFuture<Boolean> verifySignature(String signature, byte[] message, boolean prefixed) {
+//        byte[] messageHash = prefixed ? EthSigner.getEthereumMessageHash(message) : message;
+//
+//        String address = ecrecover(Numeric.hexStringToByteArray(signature), messageHash);
+//
+//        return CompletableFuture.completedFuture(address.equalsIgnoreCase(this.getAddress()));
+//    }
+//
+//    private static String ecrecover(byte[] signature, byte[] hash) {
+//        ECDSASignature sig = new ECDSASignature(
+//                Numeric.toBigInt(Arrays.copyOfRange(signature, 0, 32)),
+//                Numeric.toBigInt(Arrays.copyOfRange(signature, 32, 64)));
+//
+//        byte v = signature[64];
+//
+//        int recId;
+//        if (v >= 3) {
+//            recId = v - 27;
+//        } else {
+//            recId = v;
+//        }
+//
+//        BigInteger recovered = Sign.recoverFromSignature(recId, sig, hash);
+//        return "0x" + Keys.getAddress(recovered);
+//    }
+//
+//    private static Credentials generateCredentialsFromMnemonic(String mnemonic, int accountIndex) {
+//        // m/44'/60'/0'/0 derivation path
+//        int[] derivationPath = { 44 | Bip32ECKeyPair.HARDENED_BIT, 60 | Bip32ECKeyPair.HARDENED_BIT,
+//                0 | Bip32ECKeyPair.HARDENED_BIT, 0, accountIndex };
+//
+//        // Generate a BIP32 master keypair from the mnemonic phrase
+//        Bip32ECKeyPair masterKeypair = Bip32ECKeyPair.generateKeyPair(
+//                MnemonicUtils.generateSeed(mnemonic, ""));
+//
+//        // Derive the keypair using the derivation path
+//        Bip32ECKeyPair derivedKeyPair = Bip32ECKeyPair.deriveKeyPair(masterKeypair, derivationPath);
+//
+//        // Load the wallet for the derived keypair
+//        return Credentials.create(derivedKeyPair);
+//    }
+//}
+
+// ZKSync2 (Java): PrivateKeyEthSigner.java
+class PrivateKeyEthSigner: EthSigner {
+    
+}

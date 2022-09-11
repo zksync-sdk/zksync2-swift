66b0ddb9809653c038c75e7bab79d54529c3ad92
Maxim Makhun <mmakhun@vareger.com>
2022-09-11 16:37:05 -0700

    Add initial version of `EthSigner`.

--- Sources/ZKSync2/Crypto/Signer/EthSigner.swift
+++ Sources/ZKSync2/Crypto/Signer/EthSigner.swift
@@ -8,110 +8,37 @@
 import Foundation
 import web3swift
 
-//public interface EthSigner {
-//
-//    static final String MESSAGE_PREFIX = "\u0019Ethereum Signed Message:\n";
-//    static final String MESSAGE_EIP712_PREFIX = "\u0019\u0001";
-//    static final byte[] EIP1271_SUCCESS_VALUE = Numeric.hexStringToByteArray("0x1626ba7e");
-//
-//    /**
-//     * Get wallet address
-//     *
-//     * @return Address in hex string
-//     */
-//    String getAddress();
-//
-//    /**
-//     * Get EIP712 domain
-//     *
-//     * @return Eip712 domain
-//     */
-//    CompletableFuture<Eip712Domain> getDomain();
-//
-//    /**
-//     * Signs typed struct using ethereum private key by EIP-712 signature standard.
-//     *
-//     * @param <S> - EIP712 structure
-//     * @param domain - EIP712 domain
-//     * @param typedData - Object implementing EIP712 structure standard
-//     * @return Signature object
-//     */
-//    <S extends Structurable> CompletableFuture<String> signTypedData(Eip712Domain domain, S typedData);
-//
-//    /**
-//     * Verify typed EIP-712 struct standard.
-//     *
-//     * @param <S> - EIP712 structure
-//     * @param domain - EIP712 domain
-//     * @param typedData - Object implementing EIP712 structure standard
-//     * @param signature - Signature of the EIP-712 structures
-//     * @return true on verification success
-//     */
-//    <S extends Structurable> CompletableFuture<Boolean> verifyTypedData(Eip712Domain domain, S typedData, String signature);
-//
-//    /**
-//     * Sign raw message
-//     *
-//     * @param message - Message to sign
-//     * @return Signature object
-//     */
-//    CompletableFuture<String> signMessage(byte[] message);
-//
-//    /**
-//     * Sign raw message
-//     *
-//     * @param message - Message to sign
-//     * @param addPrefix - If true then add secure prefix (https://eips.ethereum.org/EIPS/eip-712)
-//     * @return Signature object
-//     */
-//    CompletableFuture<String> signMessage(byte[] message, boolean addPrefix);
-//
-//    /**
-//     * Verify signature with raw message
-//     *
-//     * @param signature - Signature object
-//     * @param message - Message to verify
-//     * @return true on verification success
-//     */
-//    CompletableFuture<Boolean> verifySignature(String signature, byte[] message);
-//
-//    /**
-//     * Verify signature with raw message
-//     *
-//     * @param signature - Signature object
-//     * @param message - Message to verify
-//     * @param prefixed - If true then add secure prefix (https://eips.ethereum.org/EIPS/eip-712)
-//     * @return true on verification success
-//     */
-//    CompletableFuture<Boolean> verifySignature(String signature, byte[] message, boolean prefixed);
-//
-//    static byte[] getEthereumMessagePrefix(int messageLength) {
-//        return MESSAGE_PREFIX.concat(String.valueOf(messageLength)).getBytes();
-//    }
-//
-//    static byte[] getEthereumMessageHash(byte[] message) {
-//        byte[] prefix = getEthereumMessagePrefix(message.length);
-//
-//        byte[] result = new byte[prefix.length + message.length];
-//        System.arraycopy(prefix, 0, result, 0, prefix.length);
-//        System.arraycopy(message, 0, result, prefix.length, message.length);
-//
-//        return Hash.sha3(result);
-//    }
-//}
-
 // ZKSync2 (Java): EthSigner.java
 // ZKSync (Swift): EthSigner.swift
 protocol EthSigner {
     
-//    // String getAddress();
-//    var address: String { get }
-//    
-//    // CompletableFuture<Eip712Domain> getDomain();
-//    var domain: Eip712Domain { get }
-//    
-//    // <S extends Structurable> CompletableFuture<String> signTypedData(Eip712Domain domain, S typedData);
-//    func signTypedData(_ domain: Eip712Domain)
+    var address: String { get }
     
-//    var domain: EIP712Domain {}
+    // TODO: Consider using `EIP712Domain`.
+    var domain: Eip712Domain { get }
+    
+    func signTypedData<S: Structurable>(_ domain: Eip712Domain,
+                                        typedData: S,
+                                        completion: @escaping (Swift.Result<String, Error>) -> Void)
+    
+    func verifyTypedData<S: Structurable>(_ domain: Eip712Domain,
+                                          typedData: S,
+                                          signature: String,
+                                          completion: @escaping (Swift.Result<Bool, Error>) -> Void)
+    
+    func signMessage(_ message: Data,
+                     completion: @escaping (Swift.Result<String, Error>) -> Void)
+    
+    func signMessage(_ message: Data,
+                     addPrefix: Bool,
+                     completion: @escaping (Swift.Result<String, Error>) -> Void)
+    
+    func verifySignature(_ signature: String,
+                         message: Data,
+                         completion: @escaping (Swift.Result<Bool, Error>) -> Void)
+    
+    func verifySignature(_ signature: String,
+                         message: Data,
+                         prefixed: Bool,
+                         completion: @escaping (Swift.Result<Bool, Error>) -> Void)
 }

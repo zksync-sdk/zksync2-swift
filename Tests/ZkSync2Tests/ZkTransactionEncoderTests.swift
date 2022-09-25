2a3096898b113cb27879a6ea078309a504e19c5a
Maxim Makhun <mmakhun@vareger.com>
2022-09-25 13:11:36 -0700

    Add initial version of `testEncodeWithdraw`.

--- Tests/ZkSync2Tests/ZkTransactionEncoderTests.swift
+++ Tests/ZkSync2Tests/ZkTransactionEncoderTests.swift
@@ -8,6 +8,7 @@
 import XCTest
 @testable import ZkSync2
 import web3swift
+import BigInt
 
 class ZkTransactionEncoderTests: XCTestCase {
     
@@ -20,7 +21,29 @@ class ZkTransactionEncoderTests: XCTestCase {
     }
     
     func testEncodeWithdraw() {
+        let inputs = [
+            ABI.Element.InOut(name: "_l1Receiver", type: .address),
+            ABI.Element.InOut(name: "_l2Token", type: .address),
+            ABI.Element.InOut(name: "_amount", type: .uint(bits: 256))
+        ]
         
+        let function = ABI.Element.Function(name: "withdraw",
+                                            inputs: inputs,
+                                            outputs: [],
+                                            constant: false,
+                                            payable: false)
+        
+        let elementFunction: ABI.Element = .function(function)
+        
+        let parameters: [AnyObject] = [
+            "0x7e5f4552091a69125d5dfcb7b8c2659029395bdf" as AnyObject,
+            Token.ETH.l2Address as AnyObject,
+            BigInt("1000000000000000000") as AnyObject
+        ]
+        
+        let calldata = elementFunction.encodeParameters(parameters)
+        let expectedCalldata = "0xd9caed120000000000000000000000007e5f4552091a69125d5dfcb7b8c2659029395bdf00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000de0b6b3a7640000"
+        XCTAssertEqual(calldata?.toHexString().addHexPrefix(), expectedCalldata)
     }
     
     func testEncodeDeploy() {

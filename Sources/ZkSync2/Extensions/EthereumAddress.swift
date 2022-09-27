20340f5a6384444421d898fb48294bdba8eee307
Maxim Makhun <mmakhun@vareger.com>
2022-09-27 12:14:45 -0700

    Add `EthereumAddress` extension.

---
+++ Sources/ZkSync2/Extensions/EthereumAddress.swift
@@ -0,0 +1,13 @@
+//
+//  EthereumAddress.swift
+//  ZkSync2
+//
+//  Created by Maxim Makhun on 9/27/22.
+//
+
+import web3swift
+
+extension EthereumAddress {
+    
+    static let Default = EthereumAddress("0x0000000000000000000000000000000000000000")!
+}

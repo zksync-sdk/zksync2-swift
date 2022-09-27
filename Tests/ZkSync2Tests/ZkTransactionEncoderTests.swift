ef3be4d76b250f0601234ab151f9cd9732c1ed42
Maxim Makhun <mmakhun@vareger.com>
2022-09-27 13:04:51 -0700

    Implement simple example of `EthereumTransaction` encoding.

--- Tests/ZkSync2Tests/ZkTransactionEncoderTests.swift
+++ Tests/ZkSync2Tests/ZkTransactionEncoderTests.swift
@@ -48,6 +48,43 @@ class ZkTransactionEncoderTests: XCTestCase {
         
         let expectedCalldata = "0xd9caed120000000000000000000000007e5f4552091a69125d5dfcb7b8c2659029395bdf00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000de0b6b3a7640000"
         XCTAssertEqual(calldata.toHexString().addHexPrefix(), expectedCalldata)
+        
+        // zksync2-java
+        //        Transaction1559 transaction1559 = new Transaction1559(
+        //                0, // chainId
+        //                BigInteger.valueOf(0), // nonce
+        //                BigInteger.valueOf(0), // gasLimit
+        //                "0x8c98381FfE6229Ee9E53B6aAb784E86863f61885", // to
+        //                BigInteger.valueOf(0), // value
+        //                "", // data
+        //                BigInteger.valueOf(0), // maxPriorityFeePerGas
+        //                BigInteger.valueOf(0) // maxFeePerGas
+        //        );
+        
+        var transactionOptions = TransactionOptions.defaultOptions
+        transactionOptions.type = .eip1559
+        transactionOptions.chainID = BigUInt(0)
+        transactionOptions.nonce = .manual(BigUInt(0))
+        transactionOptions.gasLimit = .manual(BigUInt(0))
+        transactionOptions.gasPrice = nil
+        transactionOptions.to = EthereumAddress("0x8c98381FfE6229Ee9E53B6aAb784E86863f61885")!
+        transactionOptions.value = BigUInt(0)
+        transactionOptions.maxPriorityFeePerGas = .manual(BigUInt(0))
+        transactionOptions.maxFeePerGas = .manual(BigUInt(0))
+        transactionOptions.callOnBlock = nil
+        
+        let ethereumParameters = EthereumParameters(from: transactionOptions)
+        let transaction = EthereumTransaction(type: .eip1559,
+                                              to: EthereumAddress("0x8c98381FfE6229Ee9E53B6aAb784E86863f61885")!,
+                                              nonce: BigUInt(0),
+                                              chainID: BigUInt(0),
+                                              value: BigUInt(0),
+                                              data: "".data(using: .ascii)!,
+                                              parameters: ethereumParameters)
+        
+        let encodedTransaction = transaction.encode(for: .signature)?.toHexString().addHexPrefix()
+        let expectedEncodedTransaction = "0x02dd8080808080948c98381ffe6229ee9e53b6aab784e86863f618858080c0"
+        XCTAssertEqual(encodedTransaction, expectedEncodedTransaction)
     }
     
     func testEncodeDeploy() {

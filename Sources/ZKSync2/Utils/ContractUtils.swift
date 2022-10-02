faf8ccc8544438adc05f34665fd583489da0ea3b
Maxim Makhun <mmakhun@vareger.com>
2022-10-01 18:31:49 -0700

    Add `ContractUtils`.

---
+++ Sources/ZKSync2/Utils/ContractUtils.swift
@@ -0,0 +1,25 @@
+//
+//  ContractUtils.swift
+//  ZkSync2
+//
+//  Created by Maxim Makhun on 10/1/22.
+//
+
+import Foundation
+import BigInt
+import web3swift
+
+class ContractUtils {
+    
+    static func generateContractAddress(address: String, nonce: BigUInt) -> Data {
+        let fields: [AnyObject] = [
+            address,
+            nonce
+        ] as [AnyObject]
+        
+        let encoded = RLP.encode(fields)!
+        let hashed = encoded.sha3(.keccak256)
+        
+        return hashed.subdata(in: 12..<hashed.count)
+    }
+}

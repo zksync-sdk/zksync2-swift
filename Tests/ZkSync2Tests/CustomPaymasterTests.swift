24538d18d2fb316dff7ef1f727165cdb85f9ee18
Maxim Makhun <mmakhun@vareger.com>
2022-10-08 22:19:12 -0700

    Add empty `CustomAccountTests` and `CustomPaymasterTests`.

---
+++ Tests/ZkSync2Tests/CustomPaymasterTests.swift
@@ -0,0 +1,19 @@
+//
+//  CustomPaymasterTests.swift
+//  ZkSync2
+//
+//  Created by Maxim Makhun on 10/8/22.
+//
+
+import XCTest
+
+class CustomPaymasterTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        
+    }
+    
+    override func tearDownWithError() throws {
+        
+    }
+}

a059975aa102190269bf6120b21f53c71e0d2ede
Maxim Makhun <mmakhun@vareger.com>
2022-10-16 15:38:33 -0700

    Improve `ZkSyncWalletIntegrationTests` setting-up process.

--- Tests/ZkSync2Tests/ZkSyncWalletIntegrationTests.swift
+++ Tests/ZkSync2Tests/ZkSyncWalletIntegrationTests.swift
@@ -13,30 +13,36 @@ import PromiseKit
 
 class ZkSyncWalletIntegrationTests: XCTestCase {
     
-    static let L1NodeUrl = URL(string: "http://206.189.96.247:8545")!
-    static let L2NodeUrl = URL(string: "http://206.189.96.247:3050")!
+    // static let L1NodeUrl = URL(string: "http://206.189.96.247:8545")!
+    // static let L2NodeUrl = URL(string: "http://206.189.96.247:3050")!
     
-//    static let L1NodeUrl = URL(string: "https://goerli.infura.io/v3/25be7ab42c414680a5f89297f8a11a4d")!
-//    static let L2NodeUrl = URL(string: "https://zksync2-testnet.zksync.dev")!
+    static let L1NodeUrl = URL(string: "https://goerli.infura.io/v3/25be7ab42c414680a5f89297f8a11a4d")!
+    static let L2NodeUrl = URL(string: "https://zksync2-testnet.zksync.dev")!
+    
+    let credentials = Credentials(BigUInt.one)
     
     var wallet: ZKSyncWallet!
     
-    let address = "0x7e5f4552091a69125d5dfcb7b8c2659029395bdf"
-    
     override func setUpWithError() throws {
-        guard let web3 = try? Web3.new(ZkSyncWalletIntegrationTests.L2NodeUrl) else {
-            XCTFail("web3 should be valid.")
-            return
+        let expectation = expectation(description: "Expectation.")
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            
+            let zkSync = JsonRpc2_0ZkSync(ZkSyncWalletIntegrationTests.L2NodeUrl)
+            
+            let chainId = try! zkSync.web3.eth.getChainIdPromise().wait()
+            
+            let signer = PrivateKeyEthSigner(self.credentials,
+                                             chainId: chainId)
+            
+            self.wallet = ZKSyncWallet(zkSync,
+                                       ethSigner: signer,
+                                       feeToken: Token.ETH)
+            
+            expectation.fulfill()
         }
         
-        let zkSync = JsonRpc2_0ZkSync(web3,
-                                      transport: HTTPTransport(ZkSyncWalletIntegrationTests.L2NodeUrl))
-        
-        let ethSigner: EthSigner = PrivateKeyEthSigner("0x0000000000000000000000000000000000000000000000000000000000000001")
-        
-        wallet = ZKSyncWallet(web3,
-                              zkSync: zkSync,
-                              ethSigner: ethSigner)
+        wait(for: [expectation], timeout: 10.0)
     }
     
     override func tearDownWithError() throws {

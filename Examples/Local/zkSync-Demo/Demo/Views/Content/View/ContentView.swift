091c6713a1f2d97ed4d2c7637ccb554d69dec082
Bojan Nikitovic <bojan@bojans-Macbook-Pro.local>
2023-07-07 03:29:51 +0200

    Minor change

--- Examples/Local/zkSync-Demo/Demo/Views/Content/View/ContentView.swift
+++ Examples/Local/zkSync-Demo/Demo/Views/Content/View/ContentView.swift
@@ -72,7 +72,7 @@ struct ContentView: View {
                 }
             }))
             
-            PrimaryButton(viewModel: ButtonViewModel(style: PrimaryButtonStyle.error, fullWidth: true, title: "Deposit via Wallet", action: {
+            PrimaryButton(viewModel: ButtonViewModel(style: PrimaryButtonStyle.primary, fullWidth: true, title: "Deposit via Wallet", action: {
                 DispatchQueue.global().async {
                     viewModel.depositViaWallet()
                 }
@@ -90,7 +90,7 @@ struct ContentView: View {
                 }
             }))
             
-            PrimaryButton(viewModel: ButtonViewModel(style: PrimaryButtonStyle.error, fullWidth: true, title: "Withdraw", action: {
+            PrimaryButton(viewModel: ButtonViewModel(style: PrimaryButtonStyle.primary, fullWidth: true, title: "Withdraw", action: {
                 DispatchQueue.global().async {
                     viewModel.withdraw()
                 }

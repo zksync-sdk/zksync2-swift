6c39aa3c5c5afebbad57bde380ab6bb604b487c8
Bojan Nikitovic <bojan@bojans-Macbook-Pro.local>
2023-07-09 02:04:02 +0200

    Revert "Change version"
    This reverts commit 25977320ae1ea902ffd187187c36ec46c260cf49.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/zksync-sdk/web3swift.git",
-            .branch("2.6.5-zksync2")
+            .branch("v2.6.5-zksync")
         ),
         .package(
             url: "https://github.com/Alamofire/Alamofire.git",

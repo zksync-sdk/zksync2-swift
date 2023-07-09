d25905622434822fa58c305c90fbc669e193b51e
Bojan Nikitovic <bojan@bojans-Macbook-Pro.local>
2023-07-09 04:00:22 +0200

    Change version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/zksync-sdk/web3swift.git",
-            .branch("v2.6.5-zksync")
+            .branch("2.6.5-zksync2")
         ),
         .package(
             url: "https://github.com/Alamofire/Alamofire.git",

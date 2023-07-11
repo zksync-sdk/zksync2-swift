57ae85ab712bac186978df6b8a8f025ab163c9b1
Bojan Nikitovic <bojan@bojans-Macbook-Pro.local>
2023-07-11 11:18:03 +0200

    Pass parameters outside of manager classes

--- Examples/Local/zkSync-Demo/Demo/Views/Content/ViewModel/ContentViewModel.swift
+++ Examples/Local/zkSync-Demo/Demo/Views/Content/ViewModel/ContentViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import BigInt
 #if canImport(zkSync2_swift)
 import zkSync2_swift
 #endif
@@ -22,6 +23,12 @@ class ContentViewModel: ObservableObject {
     
     @Published var balance: Decimal = 0
     
+    var transferToAddress = "0x082b1BB53fE43810f646dDd71AA2AB201b4C6b04"
+    var value = BigUInt(1_000_000_000_000)
+    let smartContractAddress = "0xf3ea0f675900d6aa31b2a7a30612a70e83989052"
+    let smartAccountAddress = "0x49720d21525025522040f73da5b3992112bbec00"
+    let tokenAddress = "0xbc6b677377598a79fa1885e02df1894b05bc8b33"
+    
     func refreshBalance() {
         let balance = try! baseManager.wallet.getBalance().wait()
         
@@ -33,7 +40,7 @@ class ContentViewModel: ObservableObject {
     }
     
     func deploySmartAccount() {
-        smartAccountManager.deploySmartAccount(callback: {
+        smartAccountManager.deploySmartAccount(tokenAddress: tokenAddress, callback: {
             
         })
     }
@@ -51,19 +58,19 @@ class ContentViewModel: ObservableObject {
     }
     
     func testSmartContract() {
-        smartContractManager.testSmartContract(callback: {
+        smartContractManager.testSmartContract(smartContractAddress: smartContractAddress, callback: {
             
         })
     }
     
-    func transferViaWallet() {
-        transferManager.transferViaWallet(callback: {
+    func transfer() {
+        transferManager.transfer(toAddress: transferToAddress, value: value, callback: {
             refreshBalance()
         })
     }
     
-    func transfer() {
-        transferManager.transfer(callback: {
+    func transferViaWallet() {
+        transferManager.transferViaWallet(toAddress: transferToAddress, value: value, callback: {
             refreshBalance()
         })
     }
@@ -99,7 +106,7 @@ class ContentViewModel: ObservableObject {
     }
     
     func mintToken() {
-        tokenManager.mintToken(callback: {
+        tokenManager.mintToken(tokenAddress: tokenAddress, callback: {
             
         })
     }
@@ -111,7 +118,7 @@ class ContentViewModel: ObservableObject {
     }
     
     func tokenBalance() {
-        tokenManager.tokenBalance(callback: {
+        tokenManager.tokenBalance(tokenAddress: tokenAddress, callback: {
             
         })
     }

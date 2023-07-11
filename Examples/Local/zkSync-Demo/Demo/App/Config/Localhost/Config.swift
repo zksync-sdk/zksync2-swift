51eaae3f0d2c5f8679008a38714a85f04c40cbe7
Bojan Nikitovic <bojan@bojans-Macbook-Pro.local>
2023-07-11 10:47:37 +0200

    Split into localhost and testnet targets

---
+++ Examples/Local/zkSync-Demo/Demo/App/Config/Localhost/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Localhost.swift
+//  zkSync-Demo
+//
+//  Created by Bojan on 11.7.23..
+//
+
+import Foundation
+
+struct Config {
+    static let privateKey = "0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110"
+    static let zkSyncProviderUrl = URL(string: "http://127.0.0.1:3050")!
+    static let ethereumProviderUrl = URL(string: "http://127.0.0.1:8545")!
+}
